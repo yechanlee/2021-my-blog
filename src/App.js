710afc7aceaa94c44bc6be195f71fe1b5f7fc427
이예찬 <firecare@naver.com>
2021-06-07 17:17:50 +0900

    2021.06.07 office web practice made a AppBar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Home, Send, Person } from "@material-ui/icons";
+import HomeWork from '@material-ui/icons/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          표사장님 안녕하십니까?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+    return (
+        <>
+            <div className="head1">
+
+            </div>
+            <AppBar position="static" name="gg1">
+                <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
+                    <Tab label="Home" icon={<HomeWork/>}/>
+                    <Tab label="Send" icon={<Send />} />
+                    <Tab label="Profile" icon={<Person />} />
+                </Tabs>
+            </AppBar>
+        </>
+    );
+};
+
+
 
 export default App;
